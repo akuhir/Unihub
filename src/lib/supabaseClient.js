50e4a3e39a0bf528470d0c8701beb5dead04da46
akuhir <nurudeensolograph@gmail.com>
2026-07-03 21:47:15 +0100

    Add files via upload

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://ggntpwuljjlptwbfvllo.supabase.co'
+const supabaseAnonKey = 'sb_publishable_Ejw5dCLiLhRppxc4Edz97Q_gBd_5ynN'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
